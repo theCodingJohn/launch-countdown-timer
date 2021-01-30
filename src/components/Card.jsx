6c1efe0ae67c5507b44772048bb7f38bfdccaa54
John Carlo Cunanan <codingjohn@outlook.com>
2021-01-30 15:25:37 +0800

    add card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className="card">
+      <span className="flipping-half"></span>
+      <h2>08</h2>
+    </div>
+  )
+}
+
+export default Card
