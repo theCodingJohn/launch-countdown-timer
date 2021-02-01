9b182da8fabf557041c5817a7d0c18da1c0e4a71
John Carlo Cunanan <codingjohn@outlook.com>
2021-02-01 21:16:56 +0800

    finish the mvp

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({card}) => {
   return (
     <div className="card">
       <span className="flipping-half"></span>
-      <h2>08</h2>
+      <h2>{card.time.toString().length === 1 ? `0` : ""}{card.time}</h2>
+      <p className="card--name">{card.name}</p>
     </div>
   )
 }
