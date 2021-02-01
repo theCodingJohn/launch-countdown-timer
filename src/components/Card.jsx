0cdb294acec332e493e2b95c1d2153b53070a9fd
John Carlo Cunanan <codingjohn@outlook.com>
2021-02-02 07:40:02 +0800

    add two semi-circles on card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const Card = ({card}) => {
   return (
     <div className="card">
+      <span className="card__bg"></span>
       <span className="flipping-half"></span>
-      <h2>{card.time.toString().length === 1 ? `0` : ""}{card.time}</h2>
-      <p className="card--name">{card.name}</p>
+      <h2>{card.time.toString().length === 1 ? `0${card.time}` : `${card.time}`}</h2>
+      <p className="card__name">{card.name}</p>
     </div>
   )
 }
